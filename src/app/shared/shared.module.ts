2bcf1163b085736912e04c78e20e6542ad1b8a03
ahmadi <fatemeh.aghaahmadi879@gmail.com>
2024-07-07 14:19:34 +0330

    reusable tabs in modal

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { AuthModalComponent } from '../user/auth-modal/auth-modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
   imports: [CommonModule, AuthModalComponent],
-  exports: [ModalComponent],
+  exports: [ModalComponent, TabsContainerComponent, TabComponent],
 })
 export class SharedModule {}
