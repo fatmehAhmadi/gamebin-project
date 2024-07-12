63a4f4665d976693cd844933a318e6053663945f
fatemeh133 <121554370+fatemeh133@users.noreply.github.com>
2024-07-12 09:53:01 +0330

    dynamic tabs count

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-  styleUrl: './tab.component.css'
+  styleUrl: './tab.component.css',
 })
 export class TabComponent {
-
+  @Input() tabTitle: string = '';
 }
--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ContentChildren,
+  QueryList,
+} from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
-  styleUrl: './tabs-container.component.css'
+  styleUrl: './tabs-container.component.css',
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterViewInit {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> =
+    new QueryList();
 
+  ngAfterViewInit() {
+    console.log(this.tabs);
+  }
 }
